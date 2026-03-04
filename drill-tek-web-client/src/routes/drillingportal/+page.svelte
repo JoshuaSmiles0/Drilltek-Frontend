03105868f6573adc9271184389c899c4311b4e28
JoshuaSmiles0 <20108881@mail.wit.ie>
2026-03-04 19:47:24 +0000

    Drillingportallanding
    Created banner component to be included on pages that require it. May
    rethink and add into the layout also as many pages will have this now.
    Created drill landing page with a back button to return to the main
    page, open buttons (not yet wired up) and an add program button (not yet
    wired up)

--- drill-tek-web-client/src/routes/drillingportal/+page.svelte
+++ drill-tek-web-client/src/routes/drillingportal/+page.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
-
+import Banner from '$lib/ui/banner.svelte';
     export let data
 
 </script>
 
-
-
-<h1>this is the page</h1>
-
+<Banner title="Drilling Portal" buttonName="Back" link="/mainportal" />
 
 {#each data.programs as program (program.programid) }
-    <h1>{program.location}</h1>
+    <div class="columns mt-2">
+    
+        <div class="column is-11">
+        <div class="box">
+                <h1 class="title is-3">{program.programid}</h1>
+                <p class="subtitle">
+                    Location - {program.location} <br>
+                    Orebody - {program.orebody} <br>
+                    Target - {program.target}
+                </p>
+            </div>
+    </div>        
+    <div class="column is-1">
+            <a class="button is-success">Open</a>
+        </div>
+    </div>
 {/each}
+
+<button class="button is-success">Add Program</button>
 
 
