bbbf6d2ed78d4198a13f4b29035fa1ac3944237c
JoshuaSmiles0 <20108881@mail.wit.ie>
2026-03-06 14:41:42 +0000

    EditDrillProgram
    Created modals for editing a drill program and adding drillholes in
    drill program details page. Created edit program form which prepopulates
    with current details via component props.

---
+++ drill-tek-web-client/src/lib/ui/editProgramForm.svelte
@@ -0,0 +1,48 @@
+                         
+ <script>
+  
+   let {program} = $props()
+
+ </script>                        
+                         
+                         
+                         
+                         <div class="field">
+                            <label class="label" for="programid">Program Id</label>
+                            <div class="control has-icons-left">
+                                <input id="programid" class="input" name="programid" type="text" placeholder="Program Id" value={program.programid} >
+                                <span class="icon is-small is-left">
+                                  <i class="fas fa-solid fa-address-card"></i>
+                                </span>
+                                </div>
+                            </div>
+
+<div class="field">
+                            <label class="label" for="orebody">Orebody</label>
+                            <div class="control has-icons-left">
+                                <input id="orebody" class="input" name="orebody" type="text" placeholder="Orebody" value={program.orebody} >
+                                <span class="icon is-small is-left">
+                                  <i class="fas fa-solid fa-location-arrow"></i>
+                                </span>
+                                </div>
+                            </div>
+
+<div class="field">
+                            <label class="label" for="location">Location</label>
+                            <div class="control has-icons-left">
+                                <input id="location" class="input" name="location" type="text" placeholder="Location" value={program.location} >
+                                <span class="icon is-small is-left">
+                                  <i class="fas fa-solid fa-map-pin"></i>
+                                </span>
+                                </div>
+                            </div>
+
+<div class="field">
+                            <label class="label" for="target">Target</label>
+                            <div class="control has-icons-left">
+                                <input id="target" class="input" name="target" type="text" placeholder="Target" value={program.target} >
+                                <span class="icon is-small is-left">
+                                  <i class="fas fa-solid fa-bullseye"></i>
+                                </span>
+                                </div>
+                            </div>
