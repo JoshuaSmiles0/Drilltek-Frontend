a15d6ddacc499f33843f3a6f6cb9717f9fd5c1de
JoshuaSmiles0 <20108881@mail.wit.ie>
2026-03-19 22:13:38 +0000

    Make Lists Reusable Components On Any Page with
    Created List component that takes data, a subtype and type as props.
    Will conditionally render a certain list depending on type and subtype
    props. More maintainable as all lists in one place. Less code on view

---
+++ drill-tek-web-client/src/lib/ui/listItem.svelte
@@ -0,0 +1,60 @@
+<script>
+
+   let {type, subtype, data} = $props()
+
+</script>
+{#if  type === "program"}
+       {#each data as program (program.programid) }
+    <div class="columns mt-2">
+    
+        <div class="column is-11">
+        <div class="box">
+                <h1 class="title is-3">{program.programid}</h1>
+                <p class="subtitle">
+                    Location - {program.location} <br>
+                    Orebody - {program.orebody} <br>
+                    Target - {program.target}
+                </p>
+            </div>
+    </div>
+    {#if subtype === "drilling"}        
+    <div class="column is-1">
+            <a href="/drillprogram/{program.programid}" class="button is-success">Open</a>
+        </div>
+           {/if}
+    {#if subtype === "logging"}
+         <div class="column is-1">
+            <a href="/drillprogramlogging/{program.programid}" class="button is-success">Open</a>
+        </div>
+    {/if}
+    </div>
+ 
+{/each}
+{/if}
+
+{#if type === "holes"}
+{#each data as drillhole (drillhole.holeid) }
+    <div class="columns mt-2">
+        <div class="column is-11">
+        <div class="box">
+                <h1 class="title is-3">DDH-{drillhole.holeid}</h1>
+                <p class="subtitle">
+                    Dip: {drillhole.dip} <br>
+                    Azimuth: {drillhole.azimuth} <br>
+                    Length: {drillhole.length}
+                </p>
+            </div>
+    </div>
+    {#if subtype === "drilling"}        
+    <div class="column is-1">
+            <a href="/drillhole/{drillhole.holeid}"  class="button is-success">Open</a>
+        </div>
+            {/if}
+    {#if subtype === "logging"}
+        <div class="column is-1">
+            <a href="/drillholelithlogging/{drillhole.holeid}"  class="button is-success">Open</a>
+        </div>
+    {/if}
+    </div>
+{/each}   
+{/if}
--- drill-tek-web-client/src/routes/drillprogram/[programid]/+page.svelte
+++ drill-tek-web-client/src/routes/drillprogram/[programid]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import AddDrillholeForm from "$lib/ui/addDrillholeForm.svelte";
 import Banner from "$lib/ui/banner.svelte";
-	import EditProgramForm from "$lib/ui/editProgramForm.svelte";
+	import ListItem from "$lib/ui/listItem.svelte";
 	import Modal from "$lib/ui/modal.svelte";
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -47,23 +46,5 @@ import Banner from "$lib/ui/banner.svelte";
 </div>
 
 <h1 class="title is-3">Drillholes:</h1>
-{#each data.holes as drillhole (drillhole.holeid) }
-    <div class="columns mt-2">
-        <div class="column is-11">
-        <div class="box">
-                <h1 class="title is-3">DDH-{drillhole.holeid}</h1>
-                <p class="subtitle">
-                    Dip: {drillhole.dip} <br>
-                    Azimuth: {drillhole.azimuth} <br>
-                    Length: {drillhole.length}
-                </p>
-            </div>
-    </div>        
-    <div class="column is-1">
-            <a href="/drillhole/{drillhole.holeid}"  class="button is-success">Open</a>
-        </div>
-    </div>
-{/each}
+<ListItem type="holes" subtype="drilling" data={data.holes} />
 <Modal boolean={addDrillholeModal} type="addDrillhole" verb="add" formData="" title="Please enter your Drillhole details" action="add" />
-
-
