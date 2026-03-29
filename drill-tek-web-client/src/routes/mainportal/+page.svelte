1c86567baa159973a9406ab3530b3c9a123b0791
JoshuaSmiles0 <20108881@mail.wit.ie>
2026-03-29 12:20:07 +0100

    Updated to keep in sync with fullstack

--- drill-tek-web-client/src/routes/mainportal/+page.svelte
+++ drill-tek-web-client/src/routes/mainportal/+page.svelte
@@ -19,14 +19,19 @@
 </div>
 <h1 class="title pt-2 has-text-centered"> How can we help you today...</h1>
 
-<div class="columns mt-6 pt-6 is-centered">
+<div class="columns mt-3 pt-6 is-centered">
     <div class="column is-3  has-text-centered">
     <div class="card" id="strongShadow">
     <div class="card-header p-4">
     <h2 class="title is-2">Drilling Portal</h2>
     </div>
-    <div class="card-body p-6">
-     <p class="title is-4">Proceed to the drilling portal to manage drill programs and drill holes </p>
+    <div class="card-image">
+        <figure class="image is-4by3">
+            <img class="is-fullwidth" style="border-radius: 5%;" src="../drilling_portal.jpg" alt="drill cores">
+        </figure>
+    </div>
+    <div class="card-body p-4">
+     <p class="subtitle">Proceed to the drilling portal to manage drill programs and drill holes </p>
     </div>
     <div class="card-footer p-4">
         <button class="button is-success"><a href="/drillingportal">Enter</a></button>
@@ -39,8 +44,13 @@
     <div class="card-header p-3 pb-4">
     <h2 class="title is-2">Logging Portal</h2>
     </div>
-    <div class="card-body p-6">
-     <p class="title is-4">Proceed to the logging portal to input and view logging data on holes </p>
+     <div class="card-image">
+        <figure class="image is-4by3">
+            <img class="is-fullwidth" style="border-radius: 5%;" src="../logging_portal.jpg" alt="drill cores">
+        </figure>
+    </div>
+    <div class="card-body p-4">
+     <p class="subtitle">Proceed to the logging portal to input and view logging data on holes </p>
     </div>
     <div class="card-footer p-4">
         <button class="button is-success"><a href="/loggingportal">Enter</a></button>
@@ -50,11 +60,16 @@
     </div>
         <div class="column is-3 has-text-centered ">
     <div class="card" id="strongShadow">
-    <div class="card-header p-4">
+    <div class="card-header p-3 pb-4">
     <h2 class="title is-2">Survey Portal</h2>
     </div>
-    <div class="card-body p-6">
-     <p class="title is-4">Proceed to the survey portal to validate and commit alignment surveys </p>
+    <div class="card-image">
+        <figure class="image is-4by3">
+            <img class="is-fullwidth" style="border-radius: 5%;" src="../survey_portal.jpg" alt="drill cores">
+        </figure>
+    </div>
+    <div class="card-body p-4">
+     <p class="subtitle">Proceed to the survey portal to validate and commit alignment surveys </p>
     </div>
     <div class="card-footer p-4">
         <button class="button is-success">Enter</button>
